707208521e3c8a6e00274241f376dccb17d58dec
Blixa Morgan <blixa@projectmakeit.com>
2019-05-02 19:16:21 -0500

    V0.1

--- extension/index.js
+++ extension/index.js
@@ -1,14 +1,19 @@
 'use strict';
 const Serial = require('serialport')
+const Readline = require('@serialport/parser-readline')
 const {OBSUtility} = require('nodecg-utility-obs')
-const serial = Serial('/dev/tty-interface',{autoOpen: false});
-const Users = require('streambox-users')
+const express = require('express')
+let app = express()
+//const Users = require('streambox-users')
 
 module.exports = function (nodecg) {
   const obs = new OBSUtility(nodecg)
-  const users = new Users.Users(nodecg)
+  //const users = new Users.Users(nodecg)
   const serial = Serial('/dev/ttyACM0');
+  serial.write('\n\n\n')
   const parser = serial.pipe(new Readline())
+  let countdown = false
+  let end = false
   /*const oauth = new google.auth.OAuth2(
     nodecg.bundleConfig.youtube.client_id,
     nodecg.bundleConfig.youtube.client_secret,
@@ -26,11 +31,12 @@ module.exports = function (nodecg) {
   })
 
   function processSerial(line){
+    nodecg.log.info('Got Serial Line: '+line);
     if(line[0]=="B"){
       processInput(line[1],line[2])
-    }else if(line[0]=="C"){
+    }else if(line[0]=="C" && line.length<=6){
       processLogin(line.slice(1))
-    }else if(line=="LO"){
+    }else if(line[0]=="L" && line[1]=="O"){
       processLogout()
     }
   }
@@ -46,19 +52,59 @@ module.exports = function (nodecg) {
   }
 
   function changeShield(state){
-    serial.write("!0%d\n"%state)
+    serial.write("!B0"+state+"\n")
+    nodecg.log.info("Changing sheld to: "+state)
+    if(state==1){
+      obs.send('SetSceneItemProperties',{"scene-name":"Screen","item":"Banner Logos","visible":true})
+    }else{
+      obs.send('SetSceneItemProperties',{"scene-name":"Screen","item":"Banner Logos","visible":false})
+    }
   }
   function changeCountdown(state){
-    serial.write("!2%d\n"%state)
+    if(end){
+      serial.write("!B20\n")
+      return
+    }
+    serial.write("!B2"+state+"\n")
+    if(state==1){
+      transition('Techlahoma - Countdown',true)
+      obs.send('SetSceneItemProperties',{"scene-name":"Screen","item":"Techlahoma - Countdown","visible":true})
+      countdown = true
+    }else{
+      obs.send('SetSceneItemProperties',{"scene-name":"Screen","item":"Techlahoma - Countdown","visible":false})
+      nodecg.sendMessage('obs:transition','Cut')
+      countdown = false
+    }
   }
   function changeEnd(state){
-    serial.write("!1%d\n"%state)
+    if(countdown){
+      serial.write("!B10\n")
+      return
+    }
+    serial.write("!B1"+state+"\n")
+    if(state==1){
+      transition('Techlahoma - Ended',true)
+      end = true
+    }else{
+      nodecg.sendMessage('obs:transition','Cut')
+      end = false
+    }
   }
-  function transition(scene){
-    obs.send('SetCurrentScene',{scene-name:scene})
+  function transition(scene,force){
+    obs.send('SetPreviewScene',{"scene-name":scene}).then(()=>{
+      if((end || countdown)&&!force){
+        return
+      }
+      nodecg.sendMessage('obs:transition','Cut')
+    })
   }
   function mute(state){
-    serial.write("!7%d\n"%state)
+    serial.write("!7"+state+"\n")
+    if(state==1){
+      obs.send("SetMute",{source:"Mic",mute:true})
+    }else{
+      obs.send("SetMute",{source:"Mic",mute:false})
+    }
   }
 
   function processLogin(code){
@@ -73,12 +119,15 @@ module.exports = function (nodecg) {
     serial.write("!UG\n")
     serial.write("Test User\n")
     serial.write("\n!LI\n")
+    nodecg.sendMessage('obs:connect', {ip:'localhost',port:4444,password:''})
   }
 
   function processLogout(){
-    obs.send('SetStreamSettings',{type:"rtmp_custom",settings:{server:users.empty}})
+    //obs.send('SetStreamSettings',{type:"rtmp_custom",settings:{server:users.empty}})
     serial.write("!LO")
+    nodecg.log.info("Logging Out")
     //users.closeUser()
+    nodecg.sendMessage('obs:disconnect')
   }
 
   function triggerState(){
@@ -94,7 +143,8 @@ module.exports = function (nodecg) {
     }else if(button==2){
       changeCountdown(value)
     }else if(button==3){
-      transition("speaker") }else if(button==4){
+      transition("speaker") 
+    }else if(button==4){
       transition("speaker/slides")
     }else if(button==5){
       transition("slides")
@@ -108,6 +158,12 @@ module.exports = function (nodecg) {
   }
 
   parser.on('data',processSerial)
+  serial.write("!LO\n")
+  serial.write("!B00\n")
+  serial.write("!B10\n")
+  serial.write("!B20\n")
+  nodecg.sendMessage('obs:disconnect')
 	nodecg.log.info('Ready');
+  
 }
 
